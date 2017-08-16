284a0cf455ea11730ab548f2741066278ae58469
dongmy54 <615760433@qq.com>
2017-08-16 12:04:24 +0800

    picture uploader

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,26 @@
 class PhotosController < ApplicationController
   def index
+    @photo = Photo.find(2)
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    
+    if @photo.save
+      flash[:notice] = "#{@photo.title}图片成功创建"
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def photo_params
+      params.require(:photo).permit( :image, :title, :tag)
+    end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ApplicationRecord
   validates :title, presence: true
   validates :tag, inclusion: { in: %w(empty family animals children),
-                               message: "%{value} is not a valid tag" }
+                               message: "%{value} is not a valid tag,the tag is only empty/family/animals/children" }
+  validates :image , presence: true
+  mount_uploader :image, ImageUploader
 end
