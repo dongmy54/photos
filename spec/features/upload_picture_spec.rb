8c89d359738cc3561b376274f0f35149050030c9
dongmy54 <615760433@qq.com>
2017-09-04 19:44:16 +0800

    a part

--- spec/features/upload_picture_spec.rb
+++ spec/features/upload_picture_spec.rb
@@ -7,17 +7,17 @@ feature 'upload picture', :typer => :feature do
 
     expect(page).to have_link("Upload Photo")
     
-    click_link("Upload Photo")   # 点击上传图片链接
+    click_link("Upload Photo")            # 点击上传图片链接
 
-    within("#new_photo") do      # 填photo 表单
+    within("#new_photo") do               # 填photo 表单
       fill_in "Title", with: "test"       # 注意这里title要大写，和label中保持一致
       fill_in "Tag"  , with: "children"   # tag 同理，需要大写
     end
 
     attach_file("Image", "spec/fixtures/test.jpg")  
-    # 这里第一个参数为 上传label标签
+                                          # 这里第一个参数为上传label标签
 
-    click_button "Submit"        # 提交图片
+    click_button "Submit"        
 
     expect(page).to have_content("test图片成功创建")
   end
